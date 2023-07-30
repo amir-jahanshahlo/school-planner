7b7f2319d8fd18d636b1a289b932a65c1effdf51
amir-jahanshahlo <jahanamir023@gmail.com>
2023-07-30 16:35:23 +0330

    انجام دادن پایه  ای و درست کردن اولیه تقویم

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
 import logo from './logo.svg';
 import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import timeGridPlugin from '@fullcalendar/timegrid';
 import './App.css';
+import Calendar from './component/calender';
+
 
 function App() {
-  // Function to customize the date display
-  const dateCellContent = (arg) => {
-    return (
-      <div>
-        
-        <span>{arg.dayText}</span>
-      </div>
-    );
-  };
-
-  // Function to customize the time display
-  const eventTimeFormat = {
-    hour: 'numeric',
-    minute: '2-digit',
-    meridiem: false, // Use 24-hour time format
-  };
-
-  // Function to customize the day header text
-  const dayHeaderContent = (arg) => {
-    // Custom day names (e.g., Mon, Tue, etc.)
-    const dayNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-    const dayIndex = arg.date.getDay();
-    return (
-      <div>
-        <span>{dayNames[dayIndex]}</span>
-      </div>
-    );
-  };
-
-  const headerToolbar = {
-    center: 'title',
-    right: 'timeGridWeek,timeGridDay',
-  }
-  
-  return (
-    <div className="App">
-      <FullCalendar
-        plugins={[timeGridPlugin]}
-        initialView="timeGridWeek" // Use timeGridWeek or timeGridDay for TimeGrid view
-        headerToolbar={headerToolbar}
-        dateCellContent={dateCellContent} // Customize date display
-        eventTimeFormat={eventTimeFormat} // Customize time display
-        dayHeaderContent={dayHeaderContent} // Customize day header text
-        allDaySlot={false} // Remove the "all-day" header
-      />
+  return(
+    <div className='App'>
+      <Calendar/>
+      {/* hello */}
     </div>
-  );
+  )
+  
 }
-
 export default App;
---
+++ src/component/calender.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+function isDateToday(date) {
+  const today = new Date();
+  return (
+    date.getDate() === today.getDate()
+  );
+}
+
+function Calendar() {
+  const headerToolbar = {
+    center: "title",
+    right: "timeGridWeek,timeGridDay",
+  };
+
+  const renderHeader = (arg) => {
+    const weekday = ["S", "M", "T", "W", "T", "F", "S"];
+    const dayIndex = arg.date.getDay();
+    const isToday = isDateToday(arg.date);
+
+    return (
+      <div>
+        <div>
+          <span style={{ fontSize: "10px" }}>{weekday[dayIndex]}</span>
+        </div>
+        <span
+          style={{
+            fontSize: "14px",
+            borderRadius: "50%",
+            padding: "2px",
+            background: isToday ? "blue" : "transparent",
+            color: isToday ? "white" : "black",
+          }}
+        >
+          {`${arg.date.getDate()}`}
+        </span>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"timeGridWeek"}
+        headerToolbar={headerToolbar}
+        allDaySlot={false}
+        nowIndicator={true}
+        height={1000}
+        dayHeaderContent={renderHeader}
+      />
+    </div>
+  );
+}
+
+export default Calendar;
