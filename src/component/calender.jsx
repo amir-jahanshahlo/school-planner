67e044639e87a73e8ea97909149a48cd47896642
amir-jahanshahlo <jahanamir023@gmail.com>
2023-08-02 09:36:57 +0330

    solve the backgrond issue

--- src/component/calender.jsx
+++ src/component/calender.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -18,29 +18,53 @@ function Calendar() {
     right: "timeGridWeek,timeGridDay",
   };
 
-  const renderHeader = (arg) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const isDateSelected = (date) => {
+    return selectedDate && date.toDateString() === selectedDate.toDateString();
+  };
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    // Perform other actions when a date is clicked
+  };
+
+
+  
+
+  function renderHeader(arg) {
     const weekday = ["S", "M", "T", "W", "T", "F", "S"];
     const dayIndex = arg.date.getDay();
     const isToday = isDateToday(arg.date);
-
+  
     return (
       <div>
         <div>
           <span style={{ fontSize: "10px" }}>{weekday[dayIndex]}</span>
         </div>
-        <span
-          style={{
-            fontSize: "15px",
-            borderRadius: "50%",
-            padding: "3px",
-            background: isToday ? "blue" : "transparent",
-            color: isToday ? "white" : "black",
-          }}
-        >
-          {`${arg.date.getDate()}`}
-        </span>
+        {isToday ? (
+          <span
+            className={`selectable-circle`}
+            onClick={() => handleDateClick(arg.date)}
+          >
+            {arg.date.getDate()}
+          </span>
+        ) : (
+          <span>{arg.date.getDate()}</span>
+        )}
       </div>
     );
+  }
+  
+  
+  
+  
+  
+  // Customize the time slot duration and format for AM/PM
+  const slotDuration = "00:30:00"; // Change this to set the desired duration (30 minutes in this example)
+  const slotLabelFormat = {
+    hour: "numeric",
+    hour12: true, // Use 12-hour clock format (AM/PM)
   };
   
 
@@ -54,6 +78,10 @@ function Calendar() {
         nowIndicator={true}
         height={1000}
         dayHeaderContent={renderHeader}
+        selectable={true}
+        slotDuration={slotDuration} // Apply the custom slot duration
+        slotLabelFormat={slotLabelFormat} 
+        
       />
     </div>
   );
